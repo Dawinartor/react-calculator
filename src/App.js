f8ce2e47ddad1bdb12adb3593994347adab1d1a1
David Melamed <melameddavid19@gmail.com>
2022-12-23 01:15:00 +0100

    Update my calculator

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
 import ButtonBox from "./components/ButtonBox";
@@ -11,12 +13,73 @@ const buttonValues = [
   [0, ".", "="]
 ]
 
-const App =() => {
+const App = () => {
+
+  let [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+  });
+
+  const numClickHandler = (event) => {
+    event.preventDefault();
+    const value = event.target.innerHTML;
+
+    if(calc.num.length < 16) {
+      setCalc({
+        ...calc,
+        num:
+          calc.num === 0 && value === "0"
+            ? "0"
+            : calc.num % 1 === 0
+            ? Number(calc.num + value)
+            : calc.num + value,
+        res: !calc.sign ? 0 : calc.res,
+      });
+    }
+  };
+
+  const commaClickHandler = (event) => {
+    event.preventDefault();
+    const value = event.target.innerHTML;
+
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+    });
+  };
+
   return (
     <Wrapper>
-      <Screen value="0" />
+      <Screen value={calc.num ? calc.num : calc.res} />
       <ButtonBox>
-        <Button className="" value="0" onClick={() => {console.log("Button Clicked!");}}
+        {
+          buttonValues.flat().map((button, i) => {
+            return (
+              <Button 
+                key={i}
+                className={button === "=" ? "equals" : ""}
+                value={button}
+                onClick={
+                  button === "c"
+                  /*
+                    ? resetClickHandler
+                    : button === "+-"
+                    ? invertClickHandler
+                    : button === "%"
+                    ? percentClickHandler
+                    : button === "="
+                    ? equalsClickHandler
+                    : button === "/" || button === "X" || button === "-" || button === "+"
+                    ? signClickHandler
+                    : button === "."
+                  */
+                    ? commaClickHandler // triggered if comma is pressed
+                    : numClickHandler // triggered if buttons between 0-9 are pressed
+                } 
+              />
+            );
+        })}
       </ButtonBox>
     </Wrapper>
   );
